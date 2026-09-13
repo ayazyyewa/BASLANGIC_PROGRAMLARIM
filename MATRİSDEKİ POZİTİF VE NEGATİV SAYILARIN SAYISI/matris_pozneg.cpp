6377c0e1a38319f4d536e9bf9722603577d190e8
ayazyyewa <ayazyyewa4681@gmail.com>
2026-09-13 20:12:49 +0300

    Add files via upload

---
+++ MATRİSDEKİ POZİTİF VE NEGATİV SAYILARIN SAYISI/matris_pozneg.cpp
@@ -0,0 +1,25 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+int toplam=0,x,a=0,y=0,i,j,b,z;
+int dizi[5][6]={{2,-3,65,-4,5,6},{-99,-56,12,-13,45,14},{65,-89,9,6,45,34},{67,89,-34,51,21,31},{1,-2,-3,-4,-5,-6}};
+
+for(i=0;i<5;i++)
+{
+for(j=0;j<6;j++)
+{
+if(dizi[i][j]<0)
+{
+y++;
+}
+else
+{
+	a++;
+}
+}
+}
+cout<<"MATRISTEKI NEGATIF SAYILAR:"<<y<<"\ttanedir"<<endl;
+cout<<"MATRISTEKI POZITIF SAYILAR:"<<a<<"\ttanedir"<<endl;
+return 0;
+}
